da789a1175debf15a4fb531f8a494ce98a31d968
KiranGaikwad2020 <59598642+KiranGaikwad2020@users.noreply.github.com>
2022-03-23 08:54:28 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 class ReverseString {
 	public static void main (String[] args) {
 	
-		String str= "Geeks", nstr="";
+		String str= "Kiran", nstr="";
 		char ch;
 	
 	System.out.print("Original word: ");
-	System.out.println("Geeks"); //Example word
+	System.out.println("Kiran"); //Example word
 	
 	for (int i=0; i<str.length(); i++)
 	{
